ae3516dc7806e7e14a248ae336819f489d9337af
Mateus Sponton Marques da Silva <50504398+joeynosharefood@users.noreply.github.com>
2020-06-16 12:43:40 -0300

    Add files via upload

---
+++ Prova/P0030481921039/P0030481921039/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace P0030481921039
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxRelatorio = new System.Windows.Forms.ListBox();
+            this.btEntrada = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxRelatorio
+            // 
+            this.listBoxRelatorio.FormattingEnabled = true;
+            this.listBoxRelatorio.Location = new System.Drawing.Point(289, 12);
+            this.listBoxRelatorio.Name = "listBoxRelatorio";
+            this.listBoxRelatorio.Size = new System.Drawing.Size(394, 472);
+            this.listBoxRelatorio.TabIndex = 0;
+            // 
+            // btEntrada
+            // 
+            this.btEntrada.CausesValidation = false;
+            this.btEntrada.Location = new System.Drawing.Point(8, 8);
+            this.btEntrada.Name = "btEntrada";
+            this.btEntrada.Size = new System.Drawing.Size(275, 476);
+            this.btEntrada.TabIndex = 1;
+            this.btEntrada.Text = "Entrar com Valores";
+            this.btEntrada.UseVisualStyleBackColor = true;
+            this.btEntrada.Click += new System.EventHandler(this.btEntrada_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(695, 496);
+            this.Controls.Add(this.btEntrada);
+            this.Controls.Add(this.listBoxRelatorio);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listRelatorio;
+        private System.Windows.Forms.Button bttEntrada;
+        private System.Windows.Forms.ListBox listBoxRelatorio;
+        private System.Windows.Forms.Button btEntrada;
+    }
+}
+
---
+++ Prova/P0030481921039/P0030481921039/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030481921039
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btEntrada_Click(object sender, EventArgs e)
+        {
+            double totalGeral = 0;
+            double[,] cofre = new double[9, 4];
+            string message, tittle, defaultValue;
+            string myValue;
+
+            message = "Insira o total da semana nº";
+            tittle = "Valores do mês nº";
+            defaultValue = "";
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    myValue = Interaction.InputBox(message + (j + 1), tittle + (i + 1), defaultValue);
+
+                    if ((string)myValue == defaultValue)
+                    {
+                        Interaction.MsgBox("Nenhum valor foi adicionado",
+                        MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Erro");
+                        j--;
+                    }
+                    else
+                    {
+                        if (Double.TryParse(myValue, out cofre[i, j]))
+                        {
+                            cofre[i, j] = Double.Parse(myValue);
+                        }
+                        else
+                        {
+                            Interaction.MsgBox("Não foi inserido um valor numerico",
+                            MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Erro");
+                            j--;
+                        }
+                    }
+                }
+            }
+            listBoxRelatorio.BeginUpdate();
+            for (int i = 0; i < 9; i++)
+            {
+                double temp = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    listBoxRelatorio.Items.Add("Total do Mês :" + (i + 1) + "Total Semana" + (j + 1) + ":R$ " + cofre[i, j].ToString("N2"));
+                    totalGeral += cofre[i, j];
+                    temp += cofre[i, j];
+                }
+                listBoxRelatorio.Items.Add("Total do Mês" + (i + 1) + " : R$" + temp.ToString("N2"));
+                listBoxRelatorio.Items.Add("_____________________________________________________________");
+            }
+            listBoxRelatorio.Items.Add("Total Geral: R$ " + totalGeral.ToString("N2"));
+            listBoxRelatorio.Items.Add("______________________________________________________________");
+            listBoxRelatorio.EndUpdate();
+        }
+    }
+}
+
